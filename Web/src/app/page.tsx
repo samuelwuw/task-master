1761534224b63a96d02d04817458f51ff07f347f
samuelwuw <samuelwuw@hotmail.com>
2025-02-13 13:51:45 -0300

    add folders to create api and mobile

--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold">Next.js + Tailwind + Docker 🚀</h1>
+      <h1 className="text-4x1 font-bold">teste do samu ;)</h1>
     </div>
   );
 }
